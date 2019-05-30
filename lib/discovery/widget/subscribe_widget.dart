b98190c517d1c4bb0d32f580d29909e72f7e2805
smallLabel <1007243939@qq.com>
2019-05-30 16:41:10 +0800

    订阅页面实现

--- lib/discovery/widget/subscribe_widget.dart
+++ lib/discovery/widget/subscribe_widget.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_judou/discovery/bloc/subscribe_bloc.dart';
+import 'package:flutter_judou/index/models/judou_model.dart';
+import 'package:flutter_judou/widgets/blank.dart';
+import 'package:flutter_judou/widgets/judou_cell.dart';
+import 'package:flutter_judou/widgets/loading.dart';
 import '../../bloc_provider.dart';
+
 
 
 class DiscoverySubscribe extends StatelessWidget {
@@ -8,7 +14,61 @@ class DiscoverySubscribe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      bloc: ,
+      bloc: SubscribeBloc(),
+      child: SubscribeWidget(),
     );
   }
+}
+
+class SubscribeWidget extends StatefulWidget {
+  SubscribeWidget({Key key}) : super(key: key);
+
+  _SubscribeWidgetState createState() => _SubscribeWidgetState();
+}
+
+class _SubscribeWidgetState extends State<SubscribeWidget> with AutomaticKeepAliveClientMixin {
+
+  SubscribeBloc bloc;
+  @override
+  void initState() {
+    super.initState();
+    bloc = BlocProvider.of<SubscribeBloc>(context);
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: bloc.stream ,
+      builder: (BuildContext context, AsyncSnapshot<List<JuDouModel>> snapshot) {
+        List<JuDouModel> dataList = snapshot.data;
+        if (snapshot.connectionState != ConnectionState.active) {
+          return Center(
+            child: Loading(),
+          );
+        }
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return JuDouCell(
+              model: dataList[index],
+              tag: 'DiscoveryPageSubscribe$index',
+              divider: Blank(),
+              isCell: true,
+            );
+          },
+          itemCount: dataList.length,
+          physics: AlwaysScrollableScrollPhysics(),
+        );
+      },
+    );
+  }
+
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+  @override
+  bool get wantKeepAlive => true;
 }
