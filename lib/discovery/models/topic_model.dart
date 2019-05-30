f60381b31e36d8ba8b5e96ad75d566f4b886d3a3
smallLabel <1007243939@qq.com>
2019-05-30 16:41:01 +0800

    主题模型文件

---
+++ lib/discovery/models/topic_model.dart
@@ -0,0 +1,25 @@
+class TopicModel {
+  final String schemeUrl;
+  final String name;
+  final String uuid;
+  final String cover;
+  final String description;
+
+  TopicModel({
+    this.schemeUrl,
+    this.name,
+    this.uuid,
+    this.cover,
+    this.description
+  });
+
+  factory TopicModel.fromJson(Map<String, dynamic> json) {
+    return TopicModel(
+      schemeUrl: json['scheme_url'] as String,
+      name: json['name'] as String,
+      uuid: json['uuid'] as String,
+      cover: json['cover'] as String,
+      description: json['description'] as String
+    );
+  }
+}
